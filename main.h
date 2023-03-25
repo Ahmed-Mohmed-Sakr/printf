9de248075c093498a5cf817b0e5d37c3e52aea8e
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-03-24 23:52:20 -0700

    adding _printf, parse, p_char, p_int, p_string and _write to start implemnt and upgrade them

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*lib*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*functions*/
+int _printf(const char *format, ...);
+int parse(const char *format, va_list);
+_write(char c);
+int p_char(va_list);
+int P_int(va_list);
+int P_string(va_list)
+
+
+#endif /*MAIN_H*/
