068558bce6ff2a097a59de6d44cf4687cfde019e
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-03-27 01:37:41 -0700

    divide parse function to be less than 40 line

--- _parse.c
+++ _parse.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
 /**
- * _parse - a function that check format of printf function
+ * functionsList - contain list of functions to execte.
+ *
+ *
+ * Return: array of strct.
+ */
+struct_map *functionsList()
+{
+	/*to redirect each exist char to it's own function*/
+	static struct_map type_list[] = {
+		{"c", p_char},
+		{"s", p_string},
+		{"d", p_int},
+		{"i", p_int},
+		{"%", p_persent},
+		{NULL, NULL}
+	};
+	return (type_list);
+}
+
+/**
+ * parse - a function that check format of printf function
  * and send instruction to appropriate method
  *
  * @format: string parmaeter that hold format from printf
@@ -12,30 +32,17 @@
 
 int parse(const char *format, va_list  list)
 {
-	/*to redirect each exist char to it's own function*/
-	struct_map type_list[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"d", p_int},
-		{"i", p_int},
-		{"%", p_persent},
-		{NULL, NULL}
-		};
-
+	struct_map *type_list = functionsList();
 	int h, d, num_char = 0, find_f;
 
-	/*loop on each symbol in format*/
 	for (h = 0; format[h] != '\0'; h++)
 	{
-		/*if symbol is % it meaning that there is dataType should refer to maybe*/
 		if (format[h] == '%')
 		{
-			/*search for all exist dataType %c %s %d %i*/
 			for (d = 0; type_list[d].op != NULL; d++)
 			{
 				if (format[h + 1] == type_list[d].op[0])
 				{
-					/*redirect symbol to it's appropriate function*/
 					find_f = type_list[d].f(list);
 					if (find_f == -1)
 						return (find_f);
@@ -43,7 +50,6 @@ int parse(const char *format, va_list  list)
 					break;
 				}
 			}
-			/*%k should print as %k */
 			if (type_list[d].op == NULL && format[h + 1] != ' ')
 			{
 				if (format[h + 1] != '\0')
@@ -53,14 +59,12 @@ int parse(const char *format, va_list  list)
 					num_char += 2;
 				}
 				else
-					/* only %*/
 					return (-1);
 			}
 			h++;
 		}
 		else
 		{
-			/*normal string*/
 			_write(format[h]);
 			num_char++;
 		}
